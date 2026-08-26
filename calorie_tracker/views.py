469ac1be366757593309b8fcafe701adc5aa8441
Jblue254 <japhethkiprono2020@gmail.com>
2026-08-26 18:05:16 +0300

    feat:added a url file

--- calorie_tracker/views.py
+++ calorie_tracker/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import FoodItem
 
-# Create your views here.
+def home(request):
+    foods = FoodItem.objects.all()
+    total_calories = sum(food.calories for food in foods)
+
+    context = {
+        "foods": foods,
+        "total_calories": total_calories,
+    }
+
+    return render(request, "calorie_tracker/index.html", context)
