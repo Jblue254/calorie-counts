fdfbca1218ee4e61f78802906e2c2a75ac5b6b46
Jblue254 <japhethkiprono2020@gmail.com>
2026-08-26 18:13:33 +0300

    feat:created the base the style of how it looks

--- calorie_tracker/views.py
+++ calorie_tracker/views.py
@@ -1,13 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import FoodItem
 
 def home(request):
-    foods = FoodItem.objects.all()
-    total_calories = sum(food.calories for food in foods)
+
+    if request.method == "POST":
+        day = request.POST.get("day")
+        meal = request.POST.get("meal")
+        food = request.POST.get("food")
+        calories = request.POST.get("calories")
+
+        FoodItem.objects.create(
+            day=day,
+            meal=meal,
+            food=food,
+            calories=calories
+        )
+
+        return redirect("home")
+
+    foods = FoodItem.objects.all().order_by("-id")
+
+    total_calories = sum(
+        food.calories for food in foods
+    )
 
     context = {
         "foods": foods,
         "total_calories": total_calories,
     }
 
-    return render(request, "calorie_tracker/index.html", context)
+    return render(
+        request,
+        "calorie_tracker/index.html",
+        context
+    )
