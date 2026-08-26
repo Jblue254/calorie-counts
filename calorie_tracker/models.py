bf75d14c73eab0174c5cc1fd62be36893488f0d9
Jblue254 <japhethkiprono2020@gmail.com>
2026-08-26 18:02:42 +0300

    feat:a blueprint for your application and making migrations

--- calorie_tracker/models.py
+++ calorie_tracker/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class FoodItem(models.Model):
+    day = models.CharField(max_length=20)
+    meal = models.CharField(max_length=20)
+    food = models.CharField(max_length=100)
+    calories = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.food
